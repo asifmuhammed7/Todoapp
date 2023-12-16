aa0a4c0b0f894705da82857770fb557d7a3481fe
asif <asifmuhammedn7@gmail.com>
2023-12-16 21:58:20 +0530

    implemented add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDos,setToDos] = useState([])
+  const [toDo,setToDo] = useState('')
   return (
     <div className="app">
     <div className="mainHeading">
@@ -12,19 +15,23 @@ function App() {
       <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
     </div>
     <div className="input">
-      <input type="text" placeholder="🖊️ Add item..." />
-      <i className="fas fa-plus"></i>
+      <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+      <i onClick={()=>setToDos([...toDos,toDo])} className="fas fa-plus"></i>
     </div>
     <div className="todos">
-      <div className="todo">
+    { toDos.map((value)=>{
+      return(  <div className="todo">
         <div className="left">
           <input type="checkbox" name="" id="" />
-          <p>Rect tutorial</p>
+          <p>{value}</p>
         </div>
         <div className="right">
           <i className="fas fa-times"></i>
         </div>
-      </div>
+      </div> )
+    }) 
+     
+    }
     </div>
   </div>
   );
